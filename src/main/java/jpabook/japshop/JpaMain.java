7f2b14a55b9938b5371eb7bbf87b17ebf79c0dbf
jws3873 <jws3873@gamil.com>
2023-11-09 04:41:28 +0900

    Initial commit

---
+++ src/main/java/jpabook/japshop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.japshop;
+
+import jpabook.japshop.domain.Member;
+import jpabook.japshop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // 웹서버가 올라오는 시점에 하나만 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 사용할때마다 생성, 쓰레드간에 공유x 사용하고 버려야 한다.
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Order order = em.find(Order.class, 1L);
+
+            Member findMember = order.getMember();
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
